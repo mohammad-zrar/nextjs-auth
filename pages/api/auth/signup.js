aa865c2f5ec6d55908013133b79b99896a0922aa
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-11-03 18:14:47 +0300

    Feat(pages\api\auth\signup.js): Improve signup with unique email address

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -18,6 +18,14 @@ export default async function handler(req, res) {
 
     const db = client.db('nextjs-auth');
 
+    const existingUser = await db.collection('users').findOne({ email: email });
+
+    if (existingUser) {
+        res.status(422).json({ message: 'User exists already!' });
+        client.close();
+        return;
+    }
+
     const hashedPassword = await hashPassword(password);
 
     const result = await db.collection('users').insertOne({
@@ -26,4 +34,5 @@ export default async function handler(req, res) {
     });
 
     res.status(201).json({ message: 'Created user!' });
+    client.close();
 }
