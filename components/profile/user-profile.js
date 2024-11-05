31ea0b67122abb3d8274fa9d2cde0704a0285e06
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-11-05 19:01:57 +0300

    Feat(components\profile\user-profile.js): Add client-side page guards (route protection)

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,11 +1,27 @@
+import { useSession, getSession } from 'next-auth/react';
+
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
+import { useEffect } from 'react';
 
 function UserProfile() {
-  // Redirect away if NOT auth
+  const { data: session, status } = useSession();
+  const loading = status === 'loading';
 
+  useEffect(() => {
+    getSession().then((session) => {
+      if (!session) {
+        window.location.href = '/auth';
+      }
+    })
+  })
+
+  if (loading) {
+    return <p className={classes.profile}>Loading...</p>
+  }
   return (
     <section className={classes.profile}>
+
       <h1>Your User Profile</h1>
       <ProfileForm />
     </section>
--- pages/profile.js
+++ pages/profile.js
@@ -1,3 +1,5 @@
+
+
 import UserProfile from '../components/profile/user-profile';
 
 function ProfilePage() {
