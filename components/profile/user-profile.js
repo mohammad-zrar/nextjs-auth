777b3532e720faff4cefd43d51934d6dc6071691
Mohammad Zrar <105074619+mohammad-zrar@users.noreply.github.com>
2024-11-05 19:25:25 +0300

    Feat(pages\profile.js): Add server-side page guards

--- components/profile/user-profile.js
+++ components/profile/user-profile.js
@@ -1,24 +1,24 @@
-import { useSession, getSession } from 'next-auth/react';
+// import { useSession, getSession } from 'next-auth/react';
 
 import ProfileForm from './profile-form';
 import classes from './user-profile.module.css';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 
 function UserProfile() {
-  const { data: session, status } = useSession();
-  const loading = status === 'loading';
+  // const { data: session, status } = useSession();
+  // const loading = status === 'loading';
 
-  useEffect(() => {
-    getSession().then((session) => {
-      if (!session) {
-        window.location.href = '/auth';
-      }
-    })
-  })
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     if (!session) {
+  //       window.location.href = '/auth';
+  //     }
+  //   })
+  // }, [])
 
-  if (loading) {
-    return <p className={classes.profile}>Loading...</p>
-  }
+  // if (loading) {
+  //   return <p className={classes.profile}>Loading...</p>
+  // }
   return (
     <section className={classes.profile}>
 
